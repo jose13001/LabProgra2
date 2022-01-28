24137841055f9af3b15e1282cfe989c0dffb3daf
jose13001 <josecarlos_01_30@unitec.edu>
2022-01-28 13:51:34 -0600

    se agrega la clase animales

--- src/lab2p2_josecarlosreyes_21941105/Animales.java
+++ src/lab2p2_josecarlosreyes_21941105/Animales.java
@@ -9,14 +9,6 @@ package lab2p2_josecarlosreyes_21941105;
  *
  * @author josec
  */
-public class Lab2P2_JoseCarlosReyes_21941105 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-    }
+public class Animales {
     
 }
